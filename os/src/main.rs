7ed6467fc1e2898ec483a19ea3506839e15dcbd7
yangzhenjiang <yangzhenjiang@baijian.com>
2025-09-29 15:37:38 +0800

    ch1-2

--- os/src/main.rs
+++ os/src/main.rs
@@ -6,7 +6,20 @@ mod lang_items;
 use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub fn rust_main() -> ! {
-    // println!("Hello, world!");
+    clear_bss();
+    loop {}
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|addr| {
+        unsafe {
+            (addr as *mut u8).write_volatile(0);
+        }
+    })
 }
