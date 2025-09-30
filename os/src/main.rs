1fca88e7bbe4b9cc15a8c93b265dd4e26ab96126
yangzhenjiang159 <1983337308@qq.com>
2025-09-30 08:22:54 +0800

    ch1-addLog

--- os/src/main.rs
+++ os/src/main.rs
@@ -3,9 +3,13 @@
 #![no_main]
 
 #[macro_use]
+extern crate log;
+
+#[macro_use]
 mod console;
 mod lang_items;
 mod sbi;
+pub mod logging;
 
 use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
@@ -13,7 +17,7 @@ global_asm!(include_str!("entry.asm"));
 #[unsafe(no_mangle)]
 pub fn rust_main() -> ! {
     clear_bss();
-    println!("hello world!");
+    kernel_log_info();
     panic!("shutdown");
 }
 
@@ -24,4 +28,40 @@ pub fn clear_bss() {
         fn ebss();
     }
     (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
+
+/// kernel log info
+fn kernel_log_info() {
+    unsafe extern "C" {
+        fn stext(); // begin addr of text segment
+        fn etext(); // end addr of text segment
+        fn srodata(); // start addr of Read-Only data segment
+        fn erodata(); // end addr of Read-Only data ssegment
+        fn sdata(); // start addr of data segment
+        fn edata(); // end addr of data segment
+        fn sbss(); // start addr of BSS segment
+        fn ebss(); // end addr of BSS segment
+        fn boot_stack_lower_bound(); // stack lower bound
+        fn boot_stack_top(); // stack top
+    }
+    logging::init();
+    println!("[kernel] Hello, world!");
+    trace!(
+        "[kernel] .text [{:#x}, {:#x})",
+        stext as usize,
+        etext as usize
+    );
+    debug!(
+        "[kernel] .rodata [{:#x}, {:#x})",
+        srodata as usize, erodata as usize
+    );
+    info!(
+        "[kernel] .data [{:#x}, {:#x})",
+        sdata as usize, edata as usize
+    );
+    warn!(
+        "[kernel] boot_stack top=bottom={:#x}, lower_bound={:#x}",
+        boot_stack_top as usize, boot_stack_lower_bound as usize
+    );
+    error!("[kernel] .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
 }
