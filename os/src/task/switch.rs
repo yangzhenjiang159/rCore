3466ec0dc3d273ea779ecc45a3e5dbb77079f657
yangzhenjiang <yangzhenjiang@baijian.com>
2025-11-03 10:51:40 +0800

    switch fixbug

--- os/src/task/switch.rs
+++ os/src/task/switch.rs
@@ -1,18 +1,15 @@
-//! 围绕`__switch`的Rust封装器。
+//! Rust wrapper around `__switch`.
 //!
-//! 切换到不同任务的上下文在此处进行。
-//! 实际实现不能用Rust编写，并且（本质上）必须用汇编语言编写（你知道为什么吗？），
-//! 所以这个模块实际上只是`switch.S`的一个包装器。
+//! Switching to a different task's context happens here. The actual
+//! implementation must not be in Rust and (essentially) has to be in assembly
+//! language (Do you know why?), so this module really is just a wrapper around
+//! `switch.S`.
 
+core::arch::global_asm!(include_str!("switch.S"));
 use super::TaskContext;
-use core::arch::global_asm;
 
-global_asm!(include_str!("switch.S"));
-
-unsafe extern "C" {
-    /// 切换到`next_task_cx_ptr`的上下文，并将当前上下文保存到`current_task_cx_ptr`中。
-    pub fn __switch(
-        current_task_cx_ptr: *mut TaskContext,
-        next_task_cx_ptr: *const TaskContext,
-    );
+extern "C" {
+    /// Switch to the context of `next_task_cx_ptr`, saving the current context
+    /// in `current_task_cx_ptr`.
+    pub fn __switch(current_task_cx_ptr: *mut TaskContext, next_task_cx_ptr: *const TaskContext);
 }
