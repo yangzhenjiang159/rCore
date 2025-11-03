f2af3dd5878157e7e31368aefed29ca181f091da
yangzhenjiang <yangzhenjiang@baijian.com>
2025-11-03 10:34:03 +0800

    ch4 add test

--- user/src/bin/03sleep.rs
+++ user/src/bin/03sleep.rs
@@ -6,7 +6,7 @@ extern crate user_lib;
 
 use user_lib::{get_time, yield_};
 
-#[unsafe(no_mangle)]
+#[no_mangle]
 fn main() -> i32 {
     let current_timer = get_time();
     let wait_for = current_timer + 3000;
